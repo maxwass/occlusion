1a78e29e34c6aecd8996ce4767b3eeac8f79d074
Max <maxwass@seas.upenn.edu>
2017-08-31 13:06:20 -0400

    fixed big: minHeap 'h' was given as arguement then overwritten internally. Now, not given as argument, returned from function.

--- helper.py
+++ helper.py
@@ -7,13 +7,13 @@ from shapely.geometry import Polygon
 from shapely.geometry import Point
 
 
-def nodeWeight(xy_node, xy_threat, xy_robot):
+def nodeValue(xy_node, xy_threat, xy_robot):
     #find euclidean distance between current node & robot, node & threat
-    a = numpy.subtract(xy_robot, xy_node)
+    a = numpy.subtract(xy_robot,  xy_node)
     b = numpy.subtract(xy_threat, xy_node)
 
     node2threat_dist = LA.norm(list(a))
-    node2robot_dist = LA.norm(list(b))
+    node2robot_dist  = LA.norm(list(b))
 
     #print "node2robot:  norm: %s,  vector: %s =  node %s - robot  %s" % (node2robot_dist,   a, xy_node, xy_robot)
     #print "node2threat: norm: %s   vector  %s =  node %s - threat %s" % (node2threat_dist,  b,  xy_node, xy_threat)
@@ -71,7 +71,7 @@ def shapelyPoly2plot(polygon):
     return x_coor, y_coor
 
 
-def occlusionMap(mapSize, obstacles, threatPos, robotPos, minHeap):
+def occlusionMap(mapSize, obstacles, threatPos, robotPos):
     map_width = mapSize[0];   map_height = mapSize[1]
     threat_x  = threatPos[0]; threat_y   = threatPos[1]
     robot_x   = robotPos[0];  robot_y    = robotPos[1]
@@ -81,10 +81,10 @@ def occlusionMap(mapSize, obstacles, threatPos, robotPos, minHeap):
     for x in range(0, map_width):
         for y in range(0, map_height):
             line_threat_to_vertex = LineString([(threat_x, threat_y), (x, y)])
-            #print("Vertex ( %i, %i ): " % (x, y))
 
             # calc nodes Weight for selection
-            v_weight = nodeWeight((x, y), (threatPos[0], threatPos[1]), (robot_x, robot_y))
+            v_weight = nodeValue((x, y), (threatPos[0], threatPos[1]), (robot_x, robot_y))
+            print "Vertex ( ", x, ", ", y, " ):  Value:", v_weight
 
             # check if current node is occluded from threat by any obstacle
             occluded = False
@@ -98,57 +98,33 @@ def occlusionMap(mapSize, obstacles, threatPos, robotPos, minHeap):
                 continue
             # if not occluded print green circle
             else:
+                #print (x,y)
                 hp.heappush(minHeap, (-v_weight, (x, y)))  # - because deafult is min heap
+
+    return minHeap
 
 
 
 #example scoutGoal(..., (tx,ty), (rx,ry), (mw,mh))
-def scoutGoal(rawObstacleList, threatPos, robotPos, mapSize, fig, axes, boolPlot):
+def scoutGoal(rawObstacleList, threatPos, robotPos, mapSize):
     # plotting
     map_width = mapSize[0]; map_height = mapSize[1]
 
     obstacles = []
     for raw_obs in rawObstacleList:
+        print raw_obs
         obs = Polygon(raw_obs)
         obstacles.append(obs)
 
     #build an occlusion map and find the best xy for scout
-    h = []
-    occlusionMap(mapSize, obstacles, threatPos, robotPos, h)
-
+    minHeapVertices = occlusionMap(mapSize, obstacles, threatPos, robotPos)
+    print ("returned minHeap:", len(minHeapVertices))
     # lets look at & return the 5 best candidate points
-    nsmallest = hp.nsmallest(5, h)
+    nsmallest = hp.nsmallest(5, minHeapVertices)
+    print len(nsmallest)  ###NO ELEMENTS!!!!!!!!!!
     for n in nsmallest:
         val = n[0];
         node = n[1];
         print (val, node)
 
     return nsmallest
-'''
-    # loop through every point and check if it is occluded
-    # maintain a heap of nonoccluded points
-    h = []  # list for heap
-
-    for x in range(0, map_width):
-        for y in range(0, map_height):
-            line_threat_to_vertex = LineString([(threat_x, threat_y), (x, y)])
-            #print("Vertex ( %i, %i ): " % (x, y))
-
-            # calc nodes Weight for selection
-            v_weight = nodeWeight((x, y), (threat_x, threat_y), (robot_x, robot_y))
-
-            # check if current node is occluded from threat by any obstacle
-            occluded = False
-            for obs in obstacles:
-                occluded = line_threat_to_vertex.intersects(obs)
-                if occluded:
-                    break
-
-            # if occluded then plot black circle
-            if occluded:
-                continue
-            # if not occluded print green circle
-            else:
-                hp.heappush(h, (-v_weight, (x, y)))  # - because deafult is min heap
-'''
-
--- occlusion.py
+++ occlusion.py
@@ -14,19 +14,22 @@ from shapely.geometry import Point
 from shapely.geometry import LineString
 from shapely.geometry import Polygon
 
+plot = False
 
 #plotting
 map_width = 15; map_height = 15
-plt.close()
-plt.ion()  #interactive mode: https://stackoverflow.com/questions/28269157/plotting-in-a-non-blocking-way-with-matplotlib
-plt.show()
-fig = plt.figure()
-ax = fig.add_subplot(111) # number of rows. cols, current plot number
-plt.axis('equal')
-plt.grid()
-ax.set_xlim([-3, map_width+3])
-ax.set_ylim([-3, map_height+3])
-
+'''
+if plot:
+    plt.close()
+    plt.ion()  #interactive mode: https://stackoverflow.com/questions/28269157/plotting-in-a-non-blocking-way-with-matplotlib
+    plt.show()
+    fig = plt.figure()
+    ax = fig.add_subplot(111) # number of rows. cols, current plot number
+    plt.axis('equal')
+    plt.grid()
+    ax.set_xlim([-3, map_width+3])
+    ax.set_ylim([-3, map_height+3])
+'''
 #obstacles
 ext1 = [(1,1), (1,3), (3,3), (3,1), (1,1)]
 ext2 = [(7,7), (8,8), (9,9), (8,12), (7,7)]
@@ -36,66 +39,100 @@ p1 = Polygon(ext1); p2 = Polygon(ext2); p3 = Polygon(ext3); p4 = Polygon(ext4)
 raw_obstacles = []
 raw_obstacles.append(ext1);raw_obstacles.append(ext2); raw_obstacles.append(ext3); raw_obstacles.append(ext4);
 obstacles = [p1,p3,p4]
-
-
-for obs in obstacles:
-    obs_x, obs_y = user_fun.shapelyPoly2plot(obs)
-    ax.fill(obs_x, obs_y, color="magenta", zorder=10)
-
+'''
+if plot:
+    for obs in obstacles:
+        obs_x, obs_y = user_fun.shapelyPoly2plot(obs)
+        ax.fill(obs_x, obs_y, color="magenta", zorder=10)
+'''
 
 #plot threat & robot
 threat_x = 3; threat_y = 8; robot_x = 10; robot_y = 13
-threat = plt.Circle((threat_x,threat_y), radius=.25, color='r', fill=True, zorder = 15)
-robot  = plt.Circle((robot_x, robot_y), radius=.25, color='b', fill=True, zorder = 15)
-ax.add_artist(threat); ax.add_artist(robot)
+if plot:
+    plt.close()
+    plt.ion()  # interactive mode: https://stackoverflow.com/questions/28269157/plotting-in-a-non-blocking-way-with-matplotlib
+    plt.show()
+    fig = plt.figure()
+    ax = fig.add_subplot(111)  # number of rows. cols, current plot number
+    plt.axis('equal')
+    plt.grid()
+    ax.set_xlim([-3, map_width + 3])
+    ax.set_ylim([-3, map_height + 3])
 
-#loop through every point and check if it is occluded
-#maintain a heap of nonoccluded points
+    for obs in obstacles:
+        obs_x, obs_y = user_fun.shapelyPoly2plot(obs)
+        ax.fill(obs_x, obs_y, color="magenta", zorder=10)
 
-h = [] #list for heap
+    threat = plt.Circle((threat_x,threat_y), radius=.25, color='r', fill=True, zorder = 15)
+    robot  = plt.Circle((robot_x, robot_y), radius=.25, color='b', fill=True, zorder = 15)
+    ax.add_artist(threat); ax.add_artist(robot)
 
-for x in range(0,map_width):
-    for y in range(0, map_height):
-        line_threat_to_vertex = LineString([(threat_x,threat_y),(x,y)])
-        print("Vertex ( %i, %i ): " % (x ,y))
 
-        #calc nodes Weight for selection
-        v_weight = user_fun.nodeWeight((x,y), (threat_x,threat_y), (robot_x,robot_y))
 
-        #check if current node is occluded from threat by any obstacle
-        occluded = False
-        for obs in obstacles:
-            occluded = line_threat_to_vertex.intersects(obs)
+
+
+
+if plot:
+    #loop through every point and check if it is occluded
+    #maintain a heap of nonoccluded points
+
+    h = [] #list for heap
+
+    for x in range(0,map_width):
+        for y in range(0, map_height):
+            #create a line segment connecting the threat and current node
+            #if this line intersects any of the obstacles, the node is 'occluded'
+            threat_to_vertex_line = LineString([(threat_x,threat_y),(x,y)])
+
+            #calc node's value for selection later: for later optimization, put inside 'else'
+            v_weight = user_fun.nodeValue((x,y), (threat_x,threat_y), (robot_x,robot_y))
+
+            #check if current node is occluded from threat by any obstacle
+            occluded = False
+            for obs in obstacles:
+                occluded = threat_to_vertex_line.intersects(obs)
+                if occluded:
+                    break
+
+            #if occluded then plot black circle, else print green circle and add to heap
             if occluded:
-                break # this breaks out of y loop??
-
-        #if occluded then plot black circle
-        if occluded:
-            v = plt.Circle((x,y), radius=.25, color='k', fill=True, zorder = 13)
-            ax.add_artist(v)
-            #print "     Occluded was true"
-            continue
-        #if not occluded print green circle
-        else:
-            v = plt.Circle((x,y), radius=.25, color='g', fill=True, zorder = 13)
-            plt.text(x - .3, y, str(int(v_weight)), color="blue", fontsize=8 , zorder=18)
-            ax.add_artist(v)
-            hp.heappush(h, (-v_weight, (x,y))) # - because deafult is min heap
-            #print "     Occluded was false"
+                v = plt.Circle((x,y), radius=.25, color='k', fill=True, zorder = 13)
+                ax.add_artist(v)
+                continue
+            else:
+                v = plt.Circle((x,y), radius=.25, color='g', fill=True, zorder = 13)
+                plt.text(x - .3, y, str(int(v_weight)), color="blue", fontsize=8 , zorder=18)
+                ax.add_artist(v)
+                hp.heappush(h, (-v_weight, (x,y))) # negate because min heap
 
 
-bestGoalPoints = hp.nsmallest(5,h)
-#bestGoalPoints = user_fun.scoutGoal(raw_obstacles, (threat_x, threat_y), (robot_x, robot_y), (map_width, map_height))
-for n in bestGoalPoints:
-    val = n[0]; node = n[1];
-    print node
-    l = plt.Circle((node[0], node[1]), radius=.4, color='orange', fill=False, zorder=10)
-    ax.add_artist(l)
+    bestGoalPoints = hp.nsmallest(5,h)
+    #bestGoalPoints = user_fun.scoutGoal(raw_obstacles, (threat_x, threat_y), (robot_x, robot_y), (map_width, map_height))
+    for n in bestGoalPoints:
+        val = n[0]; node = n[1];
+        print node
+        l = plt.Circle((node[0], node[1]), radius=.4, color='orange', fill=False, zorder=10)
+        ax.add_artist(l)
 
 
-plt.draw()
-plt.pause(0.001)
+    plt.draw()
+    plt.pause(0.001)
 
-# keep plot on screen for a bit
-time.sleep(25)
+    # keep plot on screen for a bit
+    time.sleep(25)
+
+
+else:
+    print "In non printing"
+    print (threat_x, threat_y)
+    print (robot_x, robot_y)
+    print (map_width, map_height)
+    bestGoalPoints = user_fun.scoutGoal(raw_obstacles, (threat_x, threat_y), (robot_x, robot_y), (map_width, map_height))
+    print bestGoalPoints
+
+
+
+
+
+
 
